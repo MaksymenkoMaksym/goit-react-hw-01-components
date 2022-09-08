7efb5d05be38727e975395dc0d3481bd5312353f
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-09-08 11:20:56 +0300

    some

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import Profile from './Profile'
+import Statistic from './Statistic'
+import user from '../users/user.json';
+import data from '../users/data.json';
 export const App = () => {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistic
+        collection={data}
+      />
+      <Statistic title="Upload stats" collection={data} />
+      {/* <Statistic stats={data} /> */}
     </div>
   );
 };
+    // <Profile
+    //   username={user.username}
+    //   tag={user.tag}
+    //   location={user.location}
+    //   avatar={user.avatar}
+    //   stats={user.stats}
+    // />
