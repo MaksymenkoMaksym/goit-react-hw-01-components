59b0fcf0b78d9a794e721cb4eddb59c50c063d24
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-09-09 18:29:15 +0300

    icons added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,8 @@
 import PropTypes from "prop-types";
+import { FaUserAstronaut, FaHashtag } from 'react-icons/fa';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+
+
 import css from './Profile.module.css'
 
 const Profile = ({ avatar, username, tag, stats: { views, likes, followers }, location }) => {
@@ -9,9 +13,10 @@ const Profile = ({ avatar, username, tag, stats: { views, likes, followers }, lo
                 alt="User avatar"
                 className={css.avatar}
             />
-            <p className={css.username}>{username}</p>
-            <p className={css.tag}>{tag}</p>
-            <p className={css.location}>{location}</p>
+            <p className={css.username}><FaUserAstronaut size={16} color={"red"} />&nbsp;{username}</p>
+            <p className={css.tag}><FaHashtag size={16} color={"red"} />&nbsp;{tag}</p>
+
+            <p className={css.location}><HiOutlineLocationMarker size={16} color={"red"} />&nbsp;{location}</p>
         </div>
 
         <ul className={css.stats}>
